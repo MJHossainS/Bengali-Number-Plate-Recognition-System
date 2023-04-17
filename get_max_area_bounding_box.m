6e6a51114a8e4e9a6262800aa9d7cddf3f0cd3b9
Mohammad Jaber Hossain <116596918+MJHossainS@users.noreply.github.com>
2023-04-17 03:18:33 +0300

    Update get_max_area_bounding_box.m

--- get_max_area_bounding_box.m
+++ get_max_area_bounding_box.m
@@ -1,13 +1,28 @@
+% This function takes an image as input and returns the bounding box of the largest connected component in the image
 function [boundingBox] = get_max_area_bounding_box(im)
-    Iprops=regionprops(im,'BoundingBox','Area', 'Image');
+
+    % Use the 'regionprops' function to get the properties of connected components in the image, including the bounding box and area
+    Iprops = regionprops(im, 'BoundingBox', 'Area', 'Image');
+
+    % Get the area of the connected components
     area = Iprops.Area;
+
+    % Count the number of connected components
     count = numel(Iprops);
-    maxa= area;
+
+    % Initialize 'maxa' to the area of the first connected component
+    maxa = area;
+
+    % Initialize 'boundingBox' to the bounding box of the first connected component
     boundingBox = Iprops.BoundingBox;
-    for i=1:count
-       if maxa<Iprops(i).Area
-           maxa=Iprops(i).Area;
-           boundingBox=Iprops(i).BoundingBox;
-       end
-    end    
+
+    % Loop through all connected components and find the one with the largest area
+    for i = 1:count
+        if maxa < Iprops(i).Area
+            % If the area of the current connected component is larger than the current maximum area, update 'maxa' and 'boundingBox'
+            maxa = Iprops(i).Area;
+            boundingBox = Iprops(i).BoundingBox;
+        end
+    end
+
 end
