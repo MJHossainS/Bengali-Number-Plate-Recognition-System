3a6ea467c616e2b61c485a3a94ff6e54d1475e46
Mohammad Jaber Hossain <116596918+MJHossainS@users.noreply.github.com>
2023-04-17 04:20:41 +0300

    Update process_and_segment_number_plate.m

--- process_and_segment_number_plate.m
+++ process_and_segment_number_plate.m
@@ -1,110 +1,135 @@
+% This function takes a binary image `imbin` and a bounding box `boundingBox` as input
 function [] = process_and_segment_number_plate(imbin, boundingBox)
+    % Load the image file data
     load imgfildata;
-    im = imcrop(imbin, boundingBox);
-    figure
-    imshow(im);
-    title('croped image');
-    %pause(2);
-    % 
-    %resize number plate to 240 NaN
-    im = imresize(im, [240 NaN]);
-    figure
-    imshow(im);
-    title('resized image');
-    %pause(2);
-    %clear dust
-    im = imopen(im, strel('rectangle', [4 4]));
-    figure
-    imshow(im);
-    title('dust removed image');
-    %pause(2);
 
+    % Crop the license plate from the original binary image using the bounding box
+    im = imcrop(imbin, boundingBox);
+
+    % Display the cropped license plate 
+    figure;
+    imshow(im);
+    title('Cropped Image');
+
+    % Resize the license plate to have a height of 240 pixels and a proportional width 
+    im = imresize(im, [240 NaN]);
+
+    % Display the resized license plate 
+    figure;
+    imshow(im);
+    title('Resized Image');
+
+    % Remove small dust particles from the license plate and a rectangle element of size 4x4
+    im = imopen(im, strel('rectangle', [4 4]));
+
+    % Display the dust-removed license plate
+    figure;
+    imshow(im);
+    title('Dust-Removed Image');
+
+    % Remove small connected objects from the inverted license plate with a threshold of 500 pixels
     im = bwareaopen(~im, 500);
 
-    figure
+    % Display the small object-removed license plate 
+    figure;
     imshow(im);
-    title('small object removed image');
+    title('Small Object-Removed Image');
 
-    % border remover
+    % Remove the border of the license plate by converting it to grayscale, thresholding it, and performing connected component analysis
     [h, w] = size(im);
-
-    picture=im;
-    if size(picture,3)==3
-      picture=rgb2gray(picture);
+    picture = im;
+    if size(picture, 3) == 3
+        picture = rgb2gray(picture);
     end
-    picture = bwareaopen(picture,30);
-    figure
+    picture = bwareaopen(picture, 30);
+    figure;
     imshow(picture);
-    %pause(2)
 
+    % Remove large connected components from the binary image using a threshold of 11000 pixels
+    picture1 = bwareaopen(picture, 11000);
+    figure;
+    imshow(picture1);
 
-    picture1=bwareaopen(picture,11000);
+    % Subtract the large connected components from the binary image to get the license plate characters
+    picture2 = picture - picture1;
+    figure;
+    imshow(picture2);
 
-    figure,imshow(picture1)
-    %pause(2)
-    picture2=picture-picture1;
-    figure,imshow(picture2)
-    %pause(2)
-    picture2=bwareaopen(picture2,200);
-    figure,imshow(picture2)
+    % Remove small connected components from the license plate characters using a threshold of 200 pixels
+    picture2 = bwareaopen(picture2, 200);
+    figure;
+    imshow(picture2);
 
-    picture2=bwareaopen(picture2,300);
-    figure,imshow(picture2)
+    % Remove small connected components from the license plate characters using a threshold of 300 pixels
+    picture2 = bwareaopen(picture2, 300);
+    figure;
+    imshow(picture2);
     title('Segmented');
 
-
-
+    % Assign the segmented image to a separate variable
     img = picture2;
-    %imshow(img, []);
+
+    % Get the size of the variable
     [nrows, ncols, dim] = size(img);
-    % Get the slices colums wise split equally
+
+    % Split the license plate into two equal slices(upper part and lower part)
     im1 = img(1:nrows/2+15,:);
     im2 = img((nrows/2)+20:2*nrows/2,:);
 
 
 
     %------------------Upper part-----------------
+    % Get the height and width of the upper part of license plate
     [h, w] = size(im1);
+
+    % Display the the upper part of license plate
     figure 
     imshow(im1);
-    %pause(2);
-    picture=im1;
-    % [~,cc]=size(picture);
-    % picture=imresize(picture,[300 500]);
 
-    [L,Ne]=bwlabel(picture);
-    propied=regionprops(L,'BoundingBox');
+    % Assign the upper part of license plate to the variable 'picture'
+    picture = im1;
+
+    % Label the connected components 
+    [L, Ne] = bwlabel(picture);
+
+    % Get the properties of the labeled regions
+    propied = regionprops(L,'BoundingBox');
+
+    % Display the bounding boxes of the regions 
     hold on
-    %pause(1)
-    for n=1:size(propied,1)
-      rectangle('Position',propied(n).BoundingBox,'EdgeColor','g','LineWidth',2)
+    for n = 1:size(propied,1)
+        rectangle('Position',propied(n).BoundingBox,'EdgeColor','g','LineWidth',2)
     end
     hold off
 
+    % Create a figure to display the segmented characters
     figure
-    final_output=[];
-    t=[];
-    for n=1:Ne
-      [r,c] = find(L==n);
-      n1=picture(min(r):max(r),min(c):max(c));
-      n1=imresize(n1,[42,24]);
-      figure
-      imshow(n1)
-    %   %pause(2)
-      x=[ ];
+    final_output = [];
+    t = [];
+    for n = 1:Ne
+        % Get the row and column indices of the labeled region
+        [r, c] = find(L == n);
 
-    totalLetters=size(imgfile,2);
+        % Crop the region from 'picture' and resize it to 42x24 pixels
+        n1 = picture(min(r):max(r),min(c):max(c));
+        n1 = imresize(n1,[42,24]);
 
+        % Display the segmented character
+        figure
+        imshow(n1)
+
+        % Compute the correlation coefficients between the segmented character and the characters in imgfildata
+        x = [];
+    totalLetters=size(imgfile,2); % Get the total number of reference images
     for k=1:totalLetters    
-       y=corr2(imgfile{1,k},n1);
-       x=[x y];    
+        y=corr2(imgfile{1,k},n1); % Compare the segmented character with each reference image using correlation
+        x=[x y]; % Store the correlation values in a vector
     end
-    t=[t max(x)];
-    if max(x)>.45
-     z=find(x==max(x));
-     out=cell2mat(imgfile(2,z));
-
-    final_output=[final_output out];
+    t=[t max(x)]; % Store the maximum correlation value for this character
+    if max(x)>.45 % Check if the maximum correlation value is greater than the threshold
+        z=find(x==max(x)); % Get the index of the reference image with the highest correlation
+        out=cell2mat(imgfile(2,z)); % Get the corresponding character label from the reference image
+    final_output=[final_output out]; % Add the character label to the final output
     end
     end
 
@@ -113,6 +138,7 @@ function [] = process_and_segment_number_plate(imbin, boundingBox)
 
 
      %------------------lower part-----------------
+     %will do similar things for the lower part of the license plate here
     [h, w] = size(im2);
     figure 
     imshow(im2);
