e54cd256256756c53a2f4e9944eadaad9143a9c8
Mohammad Jaber Hossain <116596918+MJHossainS@users.noreply.github.com>
2023-04-17 03:12:33 +0300

    Update main.m

--- main.m
+++ main.m
@@ -3,14 +3,26 @@ close all;
 clear;
 load imgfildata;
 
-[file,path]=uigetfile({'*.jpg;*.bmp;*.png;*.tif'},'Choose an image');
-s=[path,file];
-picture=imread(s);
-[~,cc]=size(picture);
-im=imresize(picture,[300 500]);
 
-figure 
+% user need to select an image file with the extensions of .jpg, .bmp, .png, or .tif
+% The selected file path is stored in the variable 'path', and the selected file name is stored in the variable 'file'
+[file, path] = uigetfile({'*.jpg;*.bmp;*.png;*.tif'}, 'Choose an image');
+% Concatenates the file name and path into a single string
+s = [path, file];
+
+% Reads the image data into the variable
+picture = imread(s);
+
+% Stores the number of columns in the variable 
+[~, cc] = size(picture);
+
+% Resizes the image to 300x500 pixels 
+im = imresize(picture, [300, 500]);
+
+% Displays the loaded image in a new figure 
+figure;
 imshow(im);
-title('Loaded Image');
+title('Loaded Image'); % Sets the title of the figure 
 
+% Calls the function 'process_images' with an argument to perform further processing
 process_images(im);
